18c8cabd138a0173a8c26516cb305e48cb418e69
18603382306 <2637752882@qq.com>
2019-11-04 14:07:41 +0800

    4

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,6 +1,8 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("ssss");
-
+        System.out.println("ssss");
+        System.out.println("ssss");
+        System.out.println("ssss");
     }
 }
