67f5827b2eeb1961fcd3571bf86e2eec93436867
18603382306 <2637752882@qq.com>
2019-11-08 13:46:59 +0800

    update

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,6 +1,18 @@
+import java.io.File;
+import java.io.InputStream;
+
 public class HelloWord {
-    public static void main(String[] args) {
-        System.out.println("ssss");
+
+    public static Workbook getWorkbok(InputStream in, File file) throws  iexception{
+        Workbook wb=null;
+        if(file.getName().endsWith(exclel_15)) {
+            wb = new HSSFWorkbook(in);
+        }else idd (file.getName().endsWith(exclel_15)){
+            wb=new XSSFWorkbook(in);
+        }
+                    return  wb;
+        }
+
 
     }
-}
+
